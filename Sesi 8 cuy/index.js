bbac1912940534fec7b855d484523696dd31eb83
81Prass <refafidela488@gmail.com>
2025-01-13 11:46:27 +0000

    Add files via upload

---
+++ Sesi 8 cuy/index.js
@@ -0,0 +1,35 @@
+let passwordLenght = document.getElementById("passwordLength");
+let password = document.getElementById("password");
+let saveBatton = document.getElementById("saveButton");
+
+
+const generatePassword = (len) => {
+const lowerAlphabet = "abcdefghijklmnopqrstuvwxyz";
+const upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYXZ";
+const numeric = "0123456789";
+const symbol = "!@#$%^&*()-+=:<>?/{}[];'";
+
+const data = lowerAlphabet + upperAlphabet + numeric + symbol
+let generator = '';
+for (let index = 0; index < len; index++) {
+    generator += data[~~(Math.random() * data.length)];
+}
+return generator
+}
+
+const getPassword = () => {
+    const newPassword = generatePassword(passwordLength.value)
+    password.value = newPassword
+    setTimeout(() => {
+        alert('password has been genrated!')
+    }, 1000)
+}
+
+const savePassword = () => {
+    document.title = password.value
+    saveBatton.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(`password saya: ${document.title}`))
+    saveBatton.setAttribute('download', 'MyPasswordGeneratorLOG.txt')
+    setTimeout(() => {
+        alert('berhasil disimpan...')
+    }, 1000)
+}
